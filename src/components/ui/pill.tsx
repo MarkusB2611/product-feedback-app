22ec68f2a87215f28642109f93b2787144ca2450
Markus Bulheller <markus.bulheller@gmail.com>
2023-02-25 03:07:17 +0100

    reusable Pill Component

---
+++ src/components/ui/pill.tsx
@@ -0,0 +1,22 @@
+const Pill = ({ option, filter, onClick }: PillProps) => {
+  return (
+    <button
+      className={`rounded-[10px] px-4 pt-[5px] pb-[6px] text-body3  hover:bg-[#CFD7FF] hover:text-[#4661E6]  ${
+        filter === option.value
+          ? "bg-[#4661E6] text-white"
+          : "bg-[#F2F4FF] text-[#4661E6]"
+      }`}
+      onClick={() => onClick(option.value)}
+    >
+      {option.label}
+    </button>
+  );
+};
+
+type PillProps = {
+  option: { value: string; label: string };
+  filter?: string;
+  onClick?: (value: string) => void;
+};
+
+export default Pill;
