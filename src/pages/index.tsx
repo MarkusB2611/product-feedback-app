3275499d47557dac11a03da9ba7be6bb2deb6374
Markus Bulheller <markus.bulheller@gmail.com>
2023-02-25 04:44:04 +0100

    suggestions header init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import Filter from "~/components/suggestions/filter";
 import RoadmapBox from "~/components/suggestions/roadmap-box";
 import TitleBoard from "~/components/suggestions/title-board";
 import DATA from "~/data.json";
+import bulbIcon from "~/assets/suggestions/icon-suggestions.svg";
+import Image from "next/image";
 
 const FILTER_OPTIONS = [
   { value: "", label: "All" },
@@ -30,7 +32,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Product feedback app" />
         <link rel="icon" href="favicon-32x32.png" />
       </Head>
-      <div className="mt-[94px] flex justify-center">
+      <div className="mt-[94px] flex justify-center gap-[30px]">
         <div className="flex w-[255px] flex-col gap-6">
           <TitleBoard />
           <Filter
@@ -46,7 +48,24 @@ const Home: NextPage = () => {
             live={suggestions.filter((s) => s.status === "live").length}
           />
         </div>
-        <main className=""></main>
+        <main className="w-[825px]">
+          <div className="flex items-center justify-between rounded-[10px] bg-[#373F68] py-[14px] pl-6 pr-4 text-h3 text-white">
+            <div className="flex items-center gap-4">
+              <Image src={bulbIcon.src} width={24} height={24} alt="" />
+              <span>
+                {suggestions.filter((s) => s.status === "suggestion").length}{" "}
+                Suggestions
+              </span>
+              {/* <div className="ml-[22px]">Sort by</div> */}
+            </div>
+            <a
+              className="rounded-[10px] bg-[#AD1FEA] px-6 py-3 text-h4 hover:bg-[#C75AF6]"
+              href="#"
+            >
+              + Add Feedback
+            </a>
+          </div>
+        </main>
       </div>
     </>
   );
